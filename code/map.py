ca2f1ee79300afc4cd23f924bc91bdb6861eb326
FelixFat <felixfat9@gmail.com>
2024-01-13 21:42:53 +0300

    Update map

--- code/map.py
+++ code/map.py
@@ -1,5 +1,9 @@
+import numpy as np
+
 from code.tiles import *
 from code.resources import TILE_FILE
+from code.level import *
+from code.resources import *
 
 
 class Map:
@@ -23,10 +27,10 @@ class Map:
     def _set_objects(self):
         self._obstacles.clear()
         self._obstacles = [
-            Block('wall', self._tilemap.map[w, h], (w, h), TILESIZE)
+            Wall(self._tilemap.map[w, h], (w, h), TILESIZE)
             for h in range(self._tilemap.map.shape[1])
             for w in range(self._tilemap.map.shape[0])
-            if self._tilemap.map[w, h] != SAND_TILE
+            if self._tilemap.map[w, h] in H1_TILES + OBSTACLES_TILES
         ]
 
     def collision(self, rect: pygame.Rect):
@@ -46,31 +50,67 @@ class Map:
                     else:
                         rect.bottom = obstacle.rect.top
 
+
 class Block:
+
     def __init__(self, in_type, tile, map_pos, size) -> None:
         self.type = in_type
         self.tile = tile
         self.rect = self._set_rect(tile, map_pos, size)
 
-    def _set_rect(self, tile, map_pos, size):
+    def _set_rect(self, map_pos, size):
         rect_pos = [map_pos[0] * size, map_pos[1] * size]
         rect_size = [size, size]
-
-        if tile == DWALL_TILE:
-            rect_pos[1] = rect_pos[1] + size // 2
-            rect_size[1] = rect_size[1] // 2
-        elif tile == RWALL_TILE:
-            rect_pos[0] = rect_pos[0] + size // 2
-            rect_size[0] = rect_size[0] // 2
-        elif tile == LWALL_TILE:
-            rect_size[0] = rect_size[0] // 2
-        elif tile == UWALL_TILE:
-            rect_size[1] = rect_size[1] // 2
-
         return pygame.Rect(rect_pos, rect_size)
     
     def get_info(self):
         return {'type': self.type, 'pos': (self.rect.x, self.rect.y), 'size': self.rect.size }
+    
+
+class Wall(Block):
+
+    def __init__(self, tile, map_pos, size) -> None:
+        super().__init__('wall', tile, map_pos, size)
+
+    def _set_rect(self, tile, map_pos, size):
+        rect_pos = [map_pos[0] * size, map_pos[1] * size]
+        rect_size = [size, size]
+
+        #if tile == DWALL_TILE:
+        #    rect_pos[1] = rect_pos[1] + size // 2
+        #    rect_size[1] = rect_size[1] // 2
+        #elif tile == RWALL_TILE:
+        #    rect_pos[0] = rect_pos[0] + size // 2
+        #    rect_size[0] = rect_size[0] // 2
+        #elif tile == LWALL_TILE:
+        #    rect_size[0] = rect_size[0] // 2
+        #elif tile == UWALL_TILE:
+        #    rect_size[1] = rect_size[1] // 2
+
+        return pygame.Rect(rect_pos, rect_size)
+
+
+def load_level(lvl_num, size):
+    w, h = size
+    map = np.array([[SAND_EMPTY_TILE for _ in range(h)] for _ in range(w)])
+
+    # col, row
+    map[:, 0] = [H1_TOP_TILE for _ in range(w)]
+    map[:, -1] = [H1_BOTTOM_TILE for _ in range(w)]
+    map[0] = [H1_LEFT_TILE for _ in range(h)]
+    map[-1] = [H1_RIGHT_TILE for _ in range(h)]
+    
+    map[0, 0] = H1_ITLCORNER_TILE
+    map[0, -1] = H1_IBLCORNER_TILE
+    map[-1, 0] = H1_ITRCORNER_TILE
+    map[-1, -1] = H1_IBRCORNER_TILE
+
+    map[9, 9] = H1_FULL_TILE
+    map[9, 10] = H1_FULL_TILE
+    map[10, 9] = H1_FULL_TILE
+    map[10, 10] = H1_FULL_TILE
+
+    return map
 
 
 def check_pos(rect: pygame.Rect):
--- code/tiles.py
+++ code/tiles.py
@@ -3,20 +3,6 @@ import numpy as np
 from code.settings import *
 
 
-SAND_TILE = 29
-
-FWALL_TILE = 9
-DWALL_TILE = 1
-RWALL_TILE = 8
-LWALL_TILE = 10
-UWALL_TILE = 17
-
-ULWALL_TILE = 19
-URWALL_TILE = 20
-DLWALL_TILE = 27
-DRWALL_TILE = 28
-
-
 class Tileset:
 
     def __init__(self, file, size, margin=1, spacing=1):
@@ -54,7 +40,7 @@ class Tilemap:
         self.tileset = tileset
         self.map = np.zeros(size, dtype=int)
 
-        h, w = self.size
+        w, h = self.size
         self.image = pygame.Surface((TILESIZE * w, TILESIZE * h))
         self.rect = self.image.get_rect() if rect is None else pygame.Rect(rect)
 
@@ -67,29 +53,6 @@ class Tilemap:
 
     def __str__(self):
         return f'{self.__class__.__name__} {self.size}'
-
-
-def load_level(lvl_num, size):
-    w, h = size
-
-    map = np.array([[SAND_TILE for _ in range(h)] for _ in range(w)])
-
-    map[:, 0] = [UWALL_TILE for _ in range(w)]
-    map[:, -1] = [DWALL_TILE for _ in range(w)]
-    map[0] = [LWALL_TILE for _ in range(h)]
-    map[-1] = [RWALL_TILE for _ in range(h)]
-    
-    map[0, 0] = ULWALL_TILE
-    map[0, -1] = DLWALL_TILE
-    map[-1, 0] = URWALL_TILE
-    map[-1, -1] = DRWALL_TILE
-
-    map[9, 9] = FWALL_TILE
-    map[9, 10] = FWALL_TILE
-    map[10, 9] = FWALL_TILE
-    map[10, 10] = FWALL_TILE
-
-    return map
 
 
 def load_image(file, shape=TILESHAPE):
